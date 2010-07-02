7352134e80def38216c2b489d3b2be8052d2e2dc
John Florian <jflorian@doubledog.org>
2010-07-01 22:08:31 -0400

    Fix - mirrmaid package not honoring LDAP account
    Added requirements to ensure that the pre-install scripts in the mirrmaid
    package will be able to successfully recognize that a mirrmaid user and group
    already exist ... in LDAP.  It is essential to do so, otherwise local accounts
    will be created and that will lead to a situation where everything looks
    perfect, yet mirrmaid will be denied write access to its targets.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,16 @@ class mirrmaid {
 
     package { "mirrmaid":
 	ensure	=> installed,
+        # These requirements are here to ensure that the pre-install scripts
+        # in the mirrmaid package will be able to successfully recognize that
+        # a mirrmaid user and group already exist ... in LDAP.  It is
+        # essential to do so, otherwise local accounts will be created and
+        # that will lead to a situation where everything looks perfect, yet
+        # mirrmaid will be denied write access to its targets.
+        require => [
+            Exec["ldap-client-configuration"],
+            Service["autofs"],
+        ],
     }
 
     file { "/etc/mirrmaid/mirrmaid.conf":
