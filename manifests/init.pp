088d03e857b3c549d40c4738f62c4d9bb2f5ee1e
John Florian <jflorian@doubledog.org>
2011-10-12 09:59:39 -0400

    Change - leverage new cron module where possible

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,10 @@
-# /etc/puppet/modules/mirrmaid/manifests/init.pp
+# modules/mirrmaid/manifests/init.pp
 
 class mirrmaid {
 
-    package { "mirrmaid":
+    include cron::daemon
+
+    package { 'mirrmaid':
 	ensure	=> installed,
         # These requirements are here to ensure that the pre-install scripts
         # in the mirrmaid package will be able to successfully recognize that
@@ -11,28 +13,25 @@ class mirrmaid {
         # that will lead to a situation where everything looks perfect, yet
         # mirrmaid will be denied write access to its targets.
         require => [
-            Exec["authconfig"],
-            Service["autofs"],
+            Exec['authconfig'],
+            Service['autofs'],
         ],
     }
 
-    file { "/etc/mirrmaid/mirrmaid.conf":
-        group	=> "mirrmaid",
-        mode    => "0640",
-        owner   => "root",
-        require => Package["mirrmaid"],
-        source  => "puppet:///modules/mirrmaid/mirrmaid.conf",
+    file { '/etc/mirrmaid/mirrmaid.conf':
+        group	=> 'mirrmaid',
+        mode    => '0640',
+        owner   => 'root',
+        require => Package['mirrmaid'],
+        source  => 'puppet:///modules/mirrmaid/mirrmaid.conf',
     }
 
-    file { "/etc/cron.d/mirrmaid":
-        group   => "root",
-        mode    => "0644",
-        owner   => "root",
+    cron:jobfile { 'mirrmaid':
         require => [
-            File["/etc/mirrmaid/mirrmaid.conf"],
-            Package["mirrmaid"],
+            File['/etc/mirrmaid/mirrmaid.conf'],
+            Package['mirrmaid'],
         ],
-        source  => "puppet:///private-host/mirrmaid/mirrmaid.cron",
+        source  => 'puppet:///private-host/mirrmaid/mirrmaid.cron',
     }
 
 }
