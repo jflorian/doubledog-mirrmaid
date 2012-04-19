56a47ffab53f3336c66c6bd1f4ef6e215b99fdcd
John Florian <jflorian@doubledog.org>
2012-04-19 17:55:42 -0400

    Change - migrate from authconfig to sssd native
    This should complete the migration of all identity/authentication
    services from sparky to peabody/krypto.
    
    NOTICE: The atrocious authconfig module is hereby deprecated (and
    removed).

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,23 +6,6 @@ class mirrmaid {
 
     package { 'mirrmaid':
 	ensure	=> installed,
-        # These requirements are here to ensure that the pre-install scripts
-        # in the mirrmaid package will be able to successfully recognize that
-        # a mirrmaid user and group already exist ... in LDAP.  It is
-        # essential to do so, otherwise local accounts will be created and
-        # that will lead to a situation where everything looks perfect, yet
-        # mirrmaid will be denied write access to its targets.
-        #
-        # Unfortunately, my authconfig class is not exactly idempotent and
-        # requires at least two catalog applications before things work.
-        # Consequently crond may balk at the mirrmaid user given to the cron
-        # job and refuse to run it.  A notify to crond might help, but that
-        # depends on a race condition; what the hell, can't hurt :(
-        notify  => Service['crond'],
-        require => [
-            Exec['authconfig'],
-            Service['autofs'],
-        ],
     }
 
     file { '/etc/mirrmaid/mirrmaid.conf':
